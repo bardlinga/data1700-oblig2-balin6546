f5edb1b53e024908083fa5702b9ae38814b33a76
bardlinga <balin6546@oslomet.no>
2024-03-05 11:41:16 +0100

    init commit

---
+++ src/main/java/com/example/data1700oblig2balin6546/Data1700Oblig2Balin6546Application.java
@@ -0,0 +1,13 @@
+package com.example.data1700oblig2balin6546;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Data1700Oblig2Balin6546Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Data1700Oblig2Balin6546Application.class, args);
+    }
+
+}
